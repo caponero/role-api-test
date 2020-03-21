be0ab8231c811c752c72db5d078493000e9c0cad
caponero <35015580+caponero@users.noreply.github.com>
2020-03-21 14:38:48 -0500

    Update test-movies.js

--- test/test-movies.js
+++ test/test-movies.js
@@ -41,10 +41,10 @@ describe('get all movies: ', ()=>{
 describe('get the movie with id ',()=>{
     it('should get the movie with id ', (done) => {
         chai.request(url)
-            .get('/movies/5e703fcc93415d0ca00f8a30')
+            .get('/movies/5e766ba8e4fb631b4c9510f7')
             .end( function(err,res){
                 // console.log(res.body)
-                expect(res.body).to.have.property('_id').to.be.equal('5e703fcc93415d0ca00f8a30');
+                expect(res.body).to.have.property('_id').to.be.equal('5e766ba8e4fb631b4c9510f7');
                 expect(res).to.have.status(200);
                 done();
         });
@@ -68,7 +68,7 @@ describe('delete the movie with id ', () => {
 describe('get the genre of movieId ',()=>{
     it('should get the genre with movieId ', (done) => {
         chai.request(url)
-            .get('/movies/5e7047f2afe1c40fc80c3d29/genre')
+            .get('/movies/5e766ba8e4fb631b4c9510f7/genre')
             .end( function(err,res){
                 //console.log(res.body)
                 expect(res.body).to.have.a('array');
@@ -82,7 +82,7 @@ describe('get the genre of movieId ',()=>{
 describe('get the genre of movieId ',()=>{
     it('should get the genre with movieId ', (done) => {
         chai.request(url)
-            .post('/movies/5e7047f2afe1c40fc80c3d29/genre')
+            .post('/movies/5e766ba8e4fb631b4c9510f7/genre')
             .send({"name": "Prueba",
                  "description": "A real great action "
                  })
